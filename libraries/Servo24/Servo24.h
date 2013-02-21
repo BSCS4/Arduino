5cc94cf76a8f2e75804de34ecb7cdb2999890aea
Misa.Z <misa@MisaZs-MacbookPro.local>
2013-02-21 22:57:44 +0800

    add comment to Servo.h

--- libraries/Servo24/Servo24.h
+++ libraries/Servo24/Servo24.h
@@ -5,16 +5,26 @@ Library for www.diy-bot.net 24 channals servo motor controller
 #include <Arduino.h>
 
 class Servo24 {
-  String serialCmd = "";
+  String serialCmd;
 public:
+
+	Servo24() {
+		serialCmd = "";
+	}
+	
 	/** Get ready to receive the command, clear the command set.*/
   void ready();
 
 	/** Set degree of servo motor, just buffer to the command set, not do it right now.
+	@param channel 1-24 channel of servo motor;
+	@param degree 1-180 degree of servo motor;
+	@return -1 for false, 0 for success.
 	*/
   int set(int channel, int degree);
   
 	/**Execute the command set.
+	@param msec set the execution time with micro-second
+	@return -1 for false, 0 for success.
 	*/
   int exec(int msec);
 };
