842b11170b52350d1ef64d799864c63c89d601d0
Misa.Z <misa@MisaZs-MacbookPro.local>
2013-02-21 23:31:32 +0800

    ...

--- libraries/Servo24/Servo24.cpp
+++ libraries/Servo24/Servo24.cpp
@@ -5,28 +5,28 @@ Library for www.diy-bot.net 24 channals servo motor controller
 #include "Servo24.h"
 
 int Servo24::set(int channel, int degree) {
-	if(channel < 1 || channel > 24) 
-		return -1;
+  if(channel < 1 || channel > 24) 
+    return -1;
 
-	if(degree < 0)
-		degree = 0;
+  if(degree < 0)
+    degree = 0;
 
-	if(degree >180)
-		degree = 180;
+  if(degree >180)
+    degree = 180;
 
-	long p = (degree * 2000l) / 180 + 500;
-	serialCmd = String(serialCmd + "#" + channel + "P" + p);
-	return 0;
+  long p = (degree * 2000l) / 180 + 500;
+  serialCmd = String(serialCmd + "#" + channel + "P" + p);
+  return 0;
 }
 
 int Servo24::exec(int msec) {
-	if(!Serial) {
-		serialCmd = "";
-		return -1;
-	}
+  if(!Serial) {
+    serialCmd = "";
+    return -1;
+  }
 
-	serialCmd = String(serialCmd + "T" + msec + "\r\n");
-	Serial.print(serialCmd);
-	serialCmd = "";
-	return 0;
+  serialCmd = String(serialCmd + "T" + msec + "\r\n");
+  Serial.print(serialCmd);
+  serialCmd = "";
+  return 0;
 }
--- libraries/Servo24/Servo24.h
+++ libraries/Servo24/Servo24.h
@@ -8,21 +8,21 @@ class Servo24 {
 String serialCmd;
 public:
 
-	Servo24() {
-		serialCmd = "";
-	}
-	
-	/** Set degree of servo motor, just buffer to the command set, not do it right now.
-	@param channel 1-24 channel of servo motor;
-	@param degree 1-180 degree of servo motor;
-	@return -1 for false, 0 for success.
-	*/
-	int set(int channel, int degree);
+  Servo24() {
+    serialCmd = "";
+  }
   
-	/**Execute the command set, and clear the command set.
-	@param msec set the execution time with micro-second
-	@return -1 for false, 0 for success.
-	*/
-	int exec(int msec);
+  /** Set degree of servo motor, just buffer to the command set, not do it right now.
+  @param channel 1-24 channel of servo motor;
+  @param degree 1-180 degree of servo motor;
+  @return -1 for false, 0 for success.
+  */
+  int set(int channel, int degree);
+  
+  /**Execute the command set, and clear the command set.
+  @param msec set the execution time with micro-second
+  @return -1 for false, 0 for success.
+  */
+  int exec(int msec);
 };
 
