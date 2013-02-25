e7bc7e4a776940d5dd08da908c38c76bcc7e2fc5
MisaZhu <misa.zhu@gmail.com>
2013-02-25 12:11:21 +0800

    rename Servo24 to ServoController

---
+++ libraries/ServoController/Servo24.h
@@ -0,0 +1,33 @@
+/**
+Library for www.diy-bot.net 24 channals servo motor controller 
+*/
+
+#ifndef SERVO_24
+#define SERVO_24
+
+#include <Arduino.h>
+
+class Servo24 {
+String serialCmd;
+public:
+
+  Servo24() {
+    serialCmd = "";
+  }
+  
+  /** Set degree of servo motor, just buffer to the command set, not do it right now.
+  @param channel 1-24 channel of servo motor;
+  @param degree 1-180 degree of servo motor;
+  @return -1 for false, 0 for success.
+  */
+  int set(int channel, int degree);
+  
+  /**Execute the command set, and clear the command set.
+  @param msec set the execution time with micro-second
+  @return -1 for false, 0 for success.
+  */
+  int exec(int msec);
+};
+
+#endif
+
